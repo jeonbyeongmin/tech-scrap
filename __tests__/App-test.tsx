549d592724355957d1de6ce18429a8a51f3fa096
jeonbyeongmin <qudals7613@gmail.com>
2022-05-28 23:22:33 +0900

    test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,13 +1,29 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
 import App from '../src/App';
-
-// Note: test renderer must be required after react-native.
+import {render} from '@testing-library/react-native';
+// import {RecoilRoot} from 'recoil';
 import renderer from 'react-test-renderer';
+
+// let props;
+// let component;
+
+// function getTempComponent(props) {
+//   return (
+//     <RecoilRoot>
+//       <App {...props} />
+//     </RecoilRoot>
+//   );
+// }
+
+// describe('[App] render', () => {
+//   props = {}; // fill test props
+//   component = getTempComponent(props);
+//   test('renders without crashing', () => {
+//     const rendered = render(component);
+//     expect(rendered).toMatchSnapshot();
+//     expect(rendered).toBeTruthy();
+//   });
+// });
 
 it('renders correctly', () => {
   renderer.create(<App />);
